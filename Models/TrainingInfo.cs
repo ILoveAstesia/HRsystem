366a7ec1cb220d1718105961400a9cfcd63fdbf9
ILA <samuraiofastesia@outlook.com>
2023-04-05 21:35:07 +0800

    减少空袭

--- Models/TrainingInfo.cs
+++ Models/TrainingInfo.cs
@@ -2,7 +2,8 @@
 {
     public class TrainingInfo
     {
-        public int Pid { get; set; }
+        public int PersonId { get; set; }
+        public int PrincipalId { get; set; }   
         public string Detail { get; set; } = string.Empty;
     }
 }
