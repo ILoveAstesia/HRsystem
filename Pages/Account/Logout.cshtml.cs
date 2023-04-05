756854b8a7198fe023ce2f6fdbde797c5dd2b7c5
ILA <samuraiofastesia@outlook.com>
2023-04-05 14:58:52 +0800

    在logout添加authorize标签

--- Pages/Account/Logout.cshtml.cs
+++ Pages/Account/Logout.cshtml.cs
@@ -2,9 +2,11 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HRsystem.Pages.Account
 {
+    [Authorize]
     public class LogoutModel : PageModel
     {
         public void OnGet()
