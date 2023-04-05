b4ab908b3e12afb87800d26e4d3a36573b6ccd41
ILA <samuraiofastesia@outlook.com>
2023-04-05 15:23:39 +0800

    使用了program中的policy做到了权限检测

--- Pages/Salary/Index.cshtml.cs
+++ Pages/Salary/Index.cshtml.cs
@@ -7,9 +7,12 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HRsystem.Data;
 using HRsystem.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
 namespace HRsystem.Pages.Salarys
 {
+    [Authorize(Policy = "AdminLest")]
     public class IndexModel : PageModel
     {
         private readonly HRsystem.Data.HRsystemContext _context;
@@ -21,12 +24,23 @@ namespace HRsystem.Pages.Salarys
 
         public IList<SalaryInfo> SalaryInfo { get;set; } = default!;
 
+        public string? Infomation { get; set; }
+
         public async Task OnGetAsync()
         {
+
+
             if (_context.SalaryInfo != null)
             {
                 SalaryInfo = await _context.SalaryInfo.ToListAsync();
             }
+
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var authority = User.FindFirst("Authority")?.Value;
+
+            Infomation=userId+ " & " + authority; 
+
+
         }
     }
 }
